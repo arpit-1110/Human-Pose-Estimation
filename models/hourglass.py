a9538656568988b5cce3eca595b36c58602e46f3
arpitaggarwal <arpit.aggarwal1999@gmail.com>
2018-12-24 15:17:53 +0000

    added hourglass level 2

--- models/hourglass.py
+++ models/hourglass.py
@@ -2,16 +2,17 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F 
 from residual import residual as res 
+import numpy as np 
 
-class stacked_hour_glass(nn.Module):
-	def __init__(self, in_channel, out_channel):
-		super(stacked_hour_glass, self).__init__()
+class hour_glass1(nn.Module):
+	def __init__(self, in_channel, out_channel, model):
+		super(hour_glass1, self).__init__()
 		self.res1 = res(in_channel, 256)
 		self.res2 = res(256, 256)
-		self.res3 = res(256, out_channel)
+		self.res3 = model
 		self.res4 = res(out_channel, out_channel)
 		self.downsample = nn.MaxPool2d(2)
-		self.upsample = nn.UpsamplingNearest2d(scale=2)
+		self.upsample = nn.UpsamplingNearest2d(scale_factor=2)
 
 	def forward(self, x):
 		out1 = self.downsample(x)
@@ -28,3 +29,19 @@ class stacked_hour_glass(nn.Module):
 
 		out = out1 + out2 
 		return out 
+
+class hour_glass2(nn.Module):
+	def __init__(self, in_channel, out_channel):
+		super(hour_glass2, self).__init__()
+		self.hg1 = hour_glass1(256, out_channel, model=res(256, out_channel))
+		self.hg2 = hour_glass1(in_channel, out_channel, model=self.hg1)
+	
+	def forward(self, x):
+		out = self.hg2(x)
+		return out
+
+if __name__ == "__main__":
+	a = torch.tensor(np.ones((1, 1, 128, 128))).float()
+	model = hour_glass2(1, 3)
+	print(model(a))
+
--- models/residual.py
+++ models/residual.py
@@ -12,19 +12,19 @@ class residual(nn.Module):
 	def __init__(self, in_channel, out_channel):
 		super(residual, self).__init__()
 		self.seq = nn.Sequential(
-			nn.BatchNorm2d(in_channel)
-			nn.ReLU(inplace=True)
-			nn.Conv2d(in_channel, out_channel//2, 1, stride=1)
-			nn.BatchNorm2d(out_channel//2)
-			nn.ReLU(inplace=True)
-			nn.Conv2d(out_channel//2, out_channel//2, 3, stride=1, padding=1)
-			nn.BatchNorm2d(out_channel//2)
-			nn.ReLU(inplace=True)
+			nn.BatchNorm2d(in_channel),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(in_channel, out_channel//2, 1, stride=1),
+			nn.BatchNorm2d(out_channel//2),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(out_channel//2, out_channel//2, 3, stride=1, padding=1),
+			nn.BatchNorm2d(out_channel//2),
+			nn.ReLU(inplace=True),
 			nn.Conv2d(out_channel//2, out_channel, 1, stride=1)
 			)
-		self.seq.apply(init_weights)
+		# self.seq.apply(init_weights)
 		self.conv = nn.Conv2d(in_channel, out_channel, 1, stride=1)
-		nn.init.xavier_uniform(conv.weight)
+		# nn.init.xavier_uniform(conv.weight)
 
 	def forward(self, x):
 		out1 = self.seq(x)
